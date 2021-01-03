8cf0f278998027671bb0dbd86e0eb6bd31e91787
Anay Nayak <anayak007@gmail.com>
2021-01-03 16:00:57 +0530

    Add border for really small states

--- src/MapChart.js
+++ src/MapChart.js
@@ -1,35 +1,81 @@
-import React, { memo } from 'react';
-import { ComposableMap, Geographies, Geography } from 'react-simple-maps';
+import React from 'react';
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  Marker
+} from 'react-simple-maps';
 import { ZoomableGroup } from 'react-simple-maps';
+import { geoArea, geoCentroid } from 'd3-geo';
+class MapChart extends React.PureComponent {
+  isSelected(geo) {
+    return geo.properties[this.props.source.prop] === this.props.selected;
+  }
 
-const MapChart = ({ selected, source }) => {
-  return (
-    <ComposableMap
-      data-tip=""
-      projection="geoMercator"
-      projectionConfig={{
-        scale: 100
-      }}
-    >
-      <ZoomableGroup zoom={source.zoom} center={source.center}>
-        <Geographies geography={source.url}>
-          {({ geographies }) =>
-            geographies.map(geo => (
-              <Geography
-                key={geo.rsmKey}
-                geography={geo}
-                fill={
-                  geo.properties[source.prop] === selected ? '#F53' : '#9998A3'
-                }
-                stroke="#EAEAEC"
-                strokeWidth=".1"
-              />
-            ))
-          }
-        </Geographies>
-      </ZoomableGroup>
-    </ComposableMap>
-  );
-};
+  isSmall(geo) {
+    console.log(geo.properties.name, geoArea(geo), geoArea(geo) < 0.0001);
+    console.log(
+      this.isSelected(geo),
+      this.props.source.prop,
+      geoArea(geo),
+      geoCentroid(this.props.source.center)
+    );
+    return this.isSelected(geo) && geoArea(geo) < 0.0001;
+  }
+  render() {
+    return (
+      <ComposableMap
+        data-tip=""
+        projection="geoMercator"
+        projectionConfig={{
+          scale: 100
+        }}
+      >
+        <ZoomableGroup
+          zoom={this.props.source.zoom}
+          center={this.props.source.center}
+        >
+          <Geographies geography={this.props.source.url}>
+            {({ geographies }) =>
+              geographies.map(geo => (
+                <Geography
+                  key={geo.rsmKey}
+                  geography={geo}
+                  fill={this.isSelected(geo) ? '#F53' : '#9998A3'}
+                  stroke="#EAEAEC"
+                  strokeWidth=".1"
+                />
+              ))
+            }
+          </Geographies>
+          <Geographies geography={this.props.source.url}>
+            {({ geographies }) =>
+              geographies.map(
+                geo =>
+                  this.isSmall(geo) && (
+                    <Marker
+                      key={`${geo.rsmKey}-m`}
+                      coordinates={geoCentroid(geo)}
+                    >
+                      <g
+                        fill="none"
+                        stroke="yellow"
+                        strokeWidth=".3"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        transform="translate(-12, -12)"
+                      >
+                        <circle cx="12" cy="12" r="2" />
+                      </g>
+                    </Marker>
+                  )
+              )
+            }
+          </Geographies>
+        </ZoomableGroup>
+      </ComposableMap>
+    );
+  }
+}
 
-export default memo(MapChart);
+export default MapChart;
